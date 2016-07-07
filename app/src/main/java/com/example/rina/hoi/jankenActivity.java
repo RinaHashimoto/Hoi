2b30b0a14a255d1aac92b9b5655b9c1ab91a4621
MarikoNoguchi <g14940nm@gm.tsuda.ac.jp>
2016-07-07 19:05:52 +0900

    じゃんけんの動作を加えました。

--- app/src/main/java/com/example/rina/hoi/jankenActivity.java
+++ app/src/main/java/com/example/rina/hoi/jankenActivity.java
@@ -12,18 +12,23 @@ public class jankenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_janken);
     }
+
+
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btngu:
                 Intent intentG = new Intent(this, jankenResultActivity.class);
+                intentG.putExtra("userHand",0);
                 startActivity(intentG);
                 break;
             case R.id.btnchoki:
                 Intent intentC = new Intent(this, jankenResultActivity.class);
+                intentC.putExtra("userHand",1);
                 startActivity(intentC);
                 break;
             case R.id.btnpa:
                 Intent intentP = new Intent(this, jankenResultActivity.class);
+                intentP.putExtra("userHand",2);
                 startActivity(intentP);
                 break;
         }
